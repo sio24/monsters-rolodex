001b17b0300b2184d728d87a975353770069cc4c
sio24 <smrcoutts24@gmail.com>
2019-08-30 13:39:27 +0200

    Initial commit

---
+++ build/precache-manifest.5bf9c66798281f2022ea235aaf192170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8882a334f4df7c1ca1a6939142bff04",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7ec512869e612ba4b7c0",
+    "url": "/monsters-rolodex/static/css/main.3634f90d.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "7ec512869e612ba4b7c0",
+    "url": "/monsters-rolodex/static/js/main.cb8624d0.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
